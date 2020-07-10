4e4c5d08702786bec18b01b82f772a2b8a9488e9
shmuelhalbfinger <40396822+shmuelhalbfinger@users.noreply.github.com>
2020-07-10 11:00:16 -0400

    Fixed Repository Annotations

--- bank/src/main/java/com/example/bank/repository/AccountRepository.java
+++ bank/src/main/java/com/example/bank/repository/AccountRepository.java
@@ -12,18 +12,22 @@ import javax.transaction.Transactional;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
     @Transactional
+    @Modifying
     @Query(value = "INSERT INTO account (username, 'name', account_balance) VALUES (:username, :name, :initialAccountBalance)", nativeQuery = true)
     public void createAccount(@Param("username") String username, @Param("name") String name, @Param("initialAccountBalance") int initialAccountBalance);
 
     @Transactional
+    @Modifying
     @Query(value = "UPDATE account SET name = :editName WHERE username = :username", nativeQuery = true)
     public void editAccountByUsername(@Param("username") String username, @Param("editName") String editName);
 
     @Transactional
+    @Modifying
     @Query(value = "UPDATE account SET account_balance = account_balance + :depositAmount WHERE username = :username", nativeQuery = true)
     public void addFundsByUsername(@Param("username") String username, @Param("depositAmount") int depositAmount);
 
     @Transactional
+    @Modifying
     @Query(value = "UPDATE account SET account_balance = account_balance - :withdrawalAmount WHERE username = :username", nativeQuery = true)
     public void subtractFundsByUsername(@Param("username") String username, @Param("withdrawalAmount") int withdrawalAmount);
 }
