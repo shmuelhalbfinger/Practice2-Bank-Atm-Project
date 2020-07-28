631916fb330905e65f3200ec38ecd07803f45440
shmuelhalbfinger <40396822+shmuelhalbfinger@users.noreply.github.com>
2020-07-28 10:06:56 -0400

    Added HTTPS capability. Still need to troubleshoot RestTemplate issue

--- atm/src/main/java/com/example/atm/service/AtmService.java
+++ atm/src/main/java/com/example/atm/service/AtmService.java
@@ -1,16 +1,66 @@
 package com.example.atm.service;
 
 import com.example.atm.model.AccountViewer;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 @Service
 public class AtmService {
 
-    private RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate = getRestTemplate();
+
+    private RestTemplate getRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        KeyStore keyStore;
+
+        HttpComponentsClientHttpRequestFactory requestFactory = null;
+
+        try {
+            keyStore = KeyStore.getInstance("PKCS12");
+            ClassPathResource classPathResource = new ClassPathResource("");
+            InputStream inputStream = classPathResource.getInputStream();
+            keyStore.load(inputStream, "keyStore".toCharArray());
+
+            SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(new SSLContextBuilder().loadTrustMaterial(null, new TrustSelfSignedStrategy()).loadKeyMaterial(keyStore, "password".toCharArray()).build(), NoopHostnameVerifier.INSTANCE);
+
+            HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).setMaxConnTotal(Integer.valueOf(5)).setMaxConnPerRoute(Integer.valueOf(5)).build();
+
+            requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+            requestFactory.setReadTimeout(Integer.valueOf(10000));
+            requestFactory.setConnectTimeout(Integer.valueOf(10000));
+
+            restTemplate.setRequestFactory(requestFactory);
+        } catch (KeyStoreException | IOException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+
+        return restTemplate;
+    }
 
     public AccountViewer deposit(String username, int depositAmount) {
         HttpHeaders headers = new HttpHeaders();
--- bank/src/test/java/com/example/bank/controller/AccountControllerTest.java
+++ bank/src/test/java/com/example/bank/controller/AccountControllerTest.java
@@ -8,17 +8,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @WebMvcTest(AccountController.class)
 @RunWith(SpringRunner.class)
@@ -36,35 +30,49 @@ public class AccountControllerTest {
     private Account account;
     private AccountEntity accountEntity;
     private ObjectMapper objectMapper;
+    private String editName;
 
     @Before
     public void setUpTests() throws Exception {
         this.objectMapper = new ObjectMapper();
-        account = objectMapper.readValue(this.setUpAccount(), Account.class);
-        accountEntity = objectMapper.readValue(this.setUpAccount(), AccountEntity.class);
-        accountEntity.setAccountId("0d3d84cd-6475-4635-bb0f-20cb8046f416");
     }
 
     @Test
     public void createAccount() throws Exception {
-        Mockito.when(accountMapper.mapToAccountEntity(Mockito.any(Account.class))).thenReturn(this.accountEntity);
-        Mockito.when(accountMapper.mapToAccount(Mockito.any(AccountEntity.class))).thenReturn(this.account);
-        Mockito.when(accountService.createAccount(this.accountEntity)).thenReturn(this.accountEntity);
-        this.mockMvc.perform(post("/account").content(this.objectMapper.writeValueAsString(this.account)).contentType("application/json")).andDo(print())
-        .andExpect(status().isOk()).andExpect(content().json(setUpAccount()));
+
     }
 
     @Test
-    public void updateAccountName() {
+    public void createAccountBad() throws Exception {
 
     }
 
-    private String setUpAccount() {
-        return "{\n" +
+    @Test
+    public void updateAccountName() throws Exception {
+    }
+
+    private void setUpAccount() throws Exception {
+        String json = "{\n" +
                 "    \"username\":\"shmuhalb\",\n" +
                 "    \"name\":\"Shmuel Halbfinger\",\n" +
                 "    \"accountBalance\":150000\n" +
                 "}";
+
+        account = objectMapper.readValue(json, Account.class);
+        accountEntity = objectMapper.readValue(json, AccountEntity.class);
+        accountEntity.setAccountId("0d3d84cd-6475-4635-bb0f-20cb8046f416");
+    }
+
+    private void setUpAccountBad() throws Exception {
+        String json = "{\n" +
+                "    \"username\":\"shmuhalb\",\n" +
+                "    \"name\":\"Shmuel Halbfinger\",\n" +
+                "    \"accountBalance\":\"Hello\"\n" +
+                "}";
+
+        account = objectMapper.readValue(json, Account.class);
+        accountEntity = objectMapper.readValue(json, AccountEntity.class);
+        accountEntity.setAccountId("0d3d84cd-6475-4635-bb0f-20cb8046f416");
     }
 
 }
