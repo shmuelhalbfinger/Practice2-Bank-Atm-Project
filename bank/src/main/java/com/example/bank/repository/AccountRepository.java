2ca989357311c156ae83cce5ab5f894015f6a9c0
shmuelhalbfinger <40396822+shmuelhalbfinger@users.noreply.github.com>
2020-07-29 14:10:07 -0400

    Added HTTPS capability. Still need to troubleshoot RestTemplate issue

--- bank/src/main/java/com/example/bank/repository/AccountRepository.java
+++ bank/src/main/java/com/example/bank/repository/AccountRepository.java
@@ -4,6 +4,7 @@ import com.example.bank.entity.AccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +12,7 @@ import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
-public interface AccountRepository extends JpaRepository<AccountEntity, String> {
+public interface AccountRepository extends JpaRepository<AccountEntity, String>, CrudRepository<AccountEntity, String> {
 
 
     public Optional<AccountEntity> findOneByUsername(String username);
